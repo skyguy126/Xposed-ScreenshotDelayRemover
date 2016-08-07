aee2c95479e4e555f31ee0a3f5b23111fc384c9e
skyguy126 <vamsieyunni@gmail.com>
2016-08-06 19:09:03 -0700

    Added new icon and source link

--- app/src/main/java/com/skyguy126/screenshotdelayremover/Shared.java
+++ app/src/main/java/com/skyguy126/screenshotdelayremover/Shared.java
@@ -3,6 +3,6 @@ package com.skyguy126.screenshotdelayremover;
 public class Shared {
     public static final String PACKAGE_NAME = BuildConfig.APPLICATION_ID;
     public static final String PREFS_FILE_NAME = "sdr_prefs";
-    public static final String SOURCE_LINK = "http://github.com";
+    public static final String SOURCE_LINK = "https://github.com/skyguy126/Xposed-ScreenshotDelayRemover";
     public static final String SUPPORT_LINK = "http://google.com";
 }
